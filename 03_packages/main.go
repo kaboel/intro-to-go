b8c7b9958873ab94aa05d8db269482bfdd504fd2
Faiq <faiq.kaboel@gmail.com>
2020-10-20 23:03:18 +0700

    packages

---
+++ 03_packages/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	//"math"
+	"github.com/kaboel/intro-to-go/03_packages/strutil"
+)
+
+func main() {
+	//fmt.Println(math.Floor(2.7))
+	//fmt.Println(math.Ceil(2.7))
+	//fmt.Println(math.Sqrt(16))
+
+	fmt.Println(strutil.Reverse("dlrow olleh"))
+}
