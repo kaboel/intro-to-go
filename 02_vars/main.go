61fb3cb51554a473f9d921972751bddc32b39513
Faiq <faiq.kaboel@gmail.com>
2020-10-20 22:36:03 +0700

    vars

---
+++ 02_vars/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	/**
+	Types:
+	- string
+	- int int8 int16 int32 int64
+	- uint uint8 uint16 int32 uint64
+	- byte > alias for uint8
+	- rune > alias for int32
+	- float32 float64
+	*/
+}
